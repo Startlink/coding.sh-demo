a6a06a8185c389fc39609931f93ffb106bea60d4
Baekjoon <greenfreakzilla@gmail.com>
2015-04-22 18:07:52 +0900

    create file/folder done

--- static/assets/app.js
+++ static/assets/app.js
@@ -5,6 +5,7 @@ var App = function() {
     var diffTop = null;
     var headerBarHeight = 35;
     var sourceInfoHeight = 22;
+    var currentFileId = null;
     function handleViewportSizeChange(first) {
         var w = $(window).width();
         var h = $(window).height();
@@ -57,6 +58,7 @@ var App = function() {
             autoCloseBrackets: true,
             theme: "lesser-dark",
             mode: "text/x-c++src",
+            readOnly: 'nocursor',
             extraKeys: {
                 'Tab': function(cm) {
                     var spaces = new Array(cm.getOption("indentUnit") + 1).join(" ");
@@ -75,6 +77,9 @@ var App = function() {
         codeMirror.on('cursorActivity', function() {
             var cur = codeMirror.getCursor();
             $('#line-info').text((cur.line+1)+':'+(cur.ch+1));
+        });
+        codeMirror.on('changes', function() {
+            saveSource();
         });
     }
     function makeDraggable() {
@@ -123,126 +128,211 @@ var App = function() {
                 diffTop = null;
             }
         });
+        $('#header-create-file-link').click(function(e) {
+            e.preventDefault();
+            createNode(null, false);
+        });
+    }
+    function convertToTreeData(elem) {
+        return {
+            text: elem.name,
+            data: elem,
+            id: elem.id,
+            parent: elem.parent,
+            type: (elem.type == 'folder' ? 'folder' : 'file'),
+            state: {
+                opened: (elem.type == 'folder' ? true : false),
+                disabled: false,
+                selected: (elem.id == 'tree-root' ? true : false),
+            }
+        };
     }
     function makeTree(data) {
         var treeData = [];
         data.forEach(function(elem, index, arr) {
-            treeData.push({
-                text: elem.name,
-                icon: (elem.type == 'folder' ? 'fa fa-folder-open-o' : 'fa fa-file-code-o tree-file'),
-                data: elem,
-                id: elem.id,
-                parent: elem.parent,
-                state: {
-                    opened: (elem.type == 'folder' ? true : false),
-                    disabled: false,
-                    selected: (elem.id == 'tree-root' ? true : false),
-                }
-            });
+            treeData.push(convertToTreeData(elem));
         });
         files = data;
         saveFiles();
         var treeConfig = {
-            'core': {
-                'data': treeData,
-                "themes" : { "name" : "default-dark" },
-                "multiple": false,
+            core: {
+                data: treeData,
+                themes : { name : "default-dark" },
+                multiple: false,
+                check_callback: function(operation, node, node_parent, node_position, more) {
+                    //console.log('rename node');
+                    //console.log(node);
+                    if (operation == 'rename_node') {
+                        return (node.data.parent != '#');
+                    }
+                }
             },
-            'plugins' : ['wholerow', 'sort', 'unique', 'contextmenu'],
-            'contextmenu': {
-                'select_node': false,
-                'show_at_node': false,
-                'items': function(o, cb) {
+            plugins : ['wholerow', 'sort', 'unique', 'contextmenu', 'types'],
+            types: {
+                folder: {
+                    icon: 'fa fa-folder-open-o',
+                    valid_children: ['folder', 'file'],
+                },
+                file: {
+                    icon: 'fa fa-file-code-o',
+                    valid_children: [],
+                },
+                '#': {
+                    max_depth: 4
+                }
+            },
+            contextmenu: {
+                select_node: false,
+                show_at_node: false,
+                items: function(o, cb) {
                     return {
-                        'create-folder': {
-                            'separator_before': false,
-                            'separator_after': false,
-                            '_disabled': false,
-                            'label': '새 폴더',
-                            'action': function(data) {
-                                createFolder(data);
+                        create_folder: {
+                            separator_before: false,
+                            separator_after: false,
+                            _disabled: false,
+                            label: '새 폴더',
+                            action: function(data) {
+                                createNode(data, true);
                             },
-                            'icon': 'fa fa-folder-o'
+                            icon: 'fa fa-folder-o'
                         },
-                        'create-file': {
-                            'separator_before': false,
-                            'separator_after': true,
-                            '_disabled': false,
-                            'label': '새 파일',
-                            'action': function(data) {
-                                createFile(data);
+                        create_file: {
+                            separator_before: false,
+                            separator_after: true,
+                            _disabled: false,
+                            label: '새 파일',
+                            action: function(data) {
+                                createNode(data, false);    
                             },
-                            'icon': 'fa fa-file-text-o'
+                            icon: 'fa fa-file-text-o'
                         },
-                        'rename-node': {
-                            'separator_before': false,
-                            'separator_after': true,
-                            '_disabled': false,
-                            'label': '이름 바꾸기',
-                            'action': function(data) {
-                                renameNode(data);
+                        rename_node: {
+                            separator_before: false,
+                            separator_after: true,
+                            _disabled: false,
+                            label: '이름 바꾸기',
+                            action: function(data) {
+                                if (isRootNode(data)) return;
+                                var inst = $.jstree.reference(data.reference),
+                                    obj = inst.get_node(data.reference);
+                                inst.edit(obj);
                             },
-                            'icon': ''
+                            icon: '',
+                            shortcut: 113,
+                            shortcut_label: 'F2'
                         },
-                        'cut-node': {
-                            'separator_before': false,
-                            'separator_after': false,
-                            '_disabled': function (data) {
+                        cut_node: {
+                            separator_before: false,
+                            separator_after: false,
+                            _disabled: function (data) {
                                 return isRootNode(data);
                             },
-                            'label': '잘라내기',
-                            'action': function(data) {
+                            label: '잘라내기',
+                            action: function(data) {
                                 cutNode(data);
                             },
-                            'icon': 'fa fa-cut',
+                            icon: 'fa fa-cut',
                         },
-                        'copy-node': {
-                            'separator_before': false,
-                            'separator_after': false,
-                            '_disabled': function (data) {
+                        copy_node: {
+                            separator_before: false,
+                            separator_after: false,
+                            _disabled: function (data) {
                                 return isRootNode(data);
                             },
-                            'label': '복사',
-                            'action': function(data) {
+                            label: '복사',
+                            action: function(data) {
                                 copyNode(data);
                             },
-                            'icon': 'fa fa-copy',
+                            icon: 'fa fa-copy',
                         },
-                        'paste-node': {
-                            'separator_before': false,
-                            'separator_after': true,
-                            '_disabled': function (data) {
+                        paste_node: {
+                            separator_before: false,
+                            separator_after: true,
+                            _disabled: function (data) {
                                 return isRootNode(data) || !$.jstree.reference(data.reference).can_paste();
                             },
-                            'label': '붙여넣기',
-                            'action': function(data) {
+                            label: '붙여넣기',
+                            action: function(data) {
                                 pasteNode(data);
                             },
-                            'icon': 'fa fa-paste',
+                            icon: 'fa fa-paste',
                         },
-                        'delete-node': {
-                            'separator_before': false,
-                            'separator_after': false,
-                            '_disabled': function(data) {
+                        delete_node: {
+                            separator_before: false,
+                            separator_after: false,
+                            _disabled: function(data) {
                                 return isRootNode(data);
                             },
-                            'label': '삭제',
-                            'action': function(data) {
+                            label: '삭제',
+                            action: function(data) {
                                 deleteNode(data);
                             },
-                            'icon': 'fa fa-trash-o'
+                            icon: 'fa fa-trash-o'
                         },
                     };
                 },
             },
+            sort: function(a, b) {
+                var u = this.get_node(a);
+                var v = this.get_node(b);
+                if (u.type != v.type) {
+                    return u.type < v.type ? 1 : -1;
+                } else {
+                    return this.get_text(a).toLowerCase() < this.get_text(b).toLowerCase() ? -1 : 1;
+                }
+            }
         };
         var tree = $('#sidebar-tree');
         tree.jstree(treeConfig);
         tree.on('changed.jstree', function(e, d) {
             if (d.node && d.node.data.type == 'file') {
-                loadFile(d.node.data.name, d.node.data.mime, d.node.data.language);
+                loadFile(d.node.data.id);
             }
         });
+        tree.on('rename_node.jstree', function(e, d) {
+            if (d.node && d.text.length) {
+                renameNode(d.node.data.id, d.text); 
+            }
+        });
+    }
+    function getInfoByExtension(ext) {
+        var temp = {'language': 'Text', 'mime': 'text/plain'};
+        if (ext) {
+            var info = CodeMirror.findModeByExtension(ext);
+            if (info) {
+                temp.language = info.name;
+                temp.mime = info.mime;
+            }
+        }
+        return temp;
+    }
+    function renameNode(nodeId, name) {
+        files.forEach(function (elem, index, arr) {
+            if (elem.id == nodeId) {
+                //var oldName = files[index].name;
+                files[index].name = name;
+                if (elem.type == 'file') {
+                    /*var m = /.+\.([^.]+)$/.exec(oldName);
+                    var oldExt = null;
+                    if (m) {
+                        oldExt = m[1];
+                    }*/
+                    var m = /.+\.([^.]+)$/.exec(name);
+                    var ext = null;
+                    var temp = getInfoByExtension();
+                    if (m) {
+                        ext = m[1];
+                        temp = getInfoByExtension(ext);
+                    }
+                    files[index].language = temp.language;
+                    files[index].mime = temp.mime;
+                    if (nodeId == currentFileId) {
+                        codeMirrorChangeMode(temp.mime, temp.language);
+                    }
+                }
+            }
+        });
+        saveFiles();
     }
     function isRootNode(data) {
         var inst = $.jstree.reference(data.reference),
@@ -262,9 +352,7 @@ var App = function() {
     }
     function pasteNode(data) {
     }
-    function loadSource(sourceCode, mime, language) {
-        var doc = CodeMirror.Doc(sourceCode, mime, 0);
-        codeMirror.swapDoc(doc);
+    function codeMirrorChangeMode(mime, language) {
         var mode,spec;
         var info = CodeMirror.findModeByMIME(mime);
         if (info) {
@@ -275,14 +363,44 @@ var App = function() {
             codeMirror.setOption('mode', spec);
             CodeMirror.autoLoadMode(codeMirror, mode);
             $('#language-info').text(language);
-            codeMirror.focus();
         }
     }
+    function loadSource(fileId, sourceCode, mime, language) {
+        currentFileId = null;
+        var doc = CodeMirror.Doc(sourceCode, mime, 0);
+        codeMirror.swapDoc(doc);
 
-    function loadFile(filename, mime, language) {
-        $.ajax('/samples/'+filename).done(function(res) {
-            loadSource(res, mime, language);
-        });
+        codeMirror.setOption('readOnly', false);
+        codeMirrorChangeMode(mime, language);
+        codeMirror.focus();
+        currentFileId = fileId;
+    }
+    function getFileById(fileId) {
+        for (var i=0; i<files.length; i++) {
+            if (files[i].id == fileId) {
+                return files[i];
+            }
+        }
+        return null;
+    }
+    function loadFile(fileId) {
+        var file = getFileById(fileId);
+        if (file === null) return;
+        if (Modernizr.localstorage && localStorage.getItem(fileId) !== null) {
+            loadSource(fileId, localStorage.getItem(fileId), file.mime, file.language);
+        } else {
+            $.ajax('/samples/'+file.name).done(function(res) {
+                if (Modernizr.localstorage) {
+                    localStorage.setItem(fileId, res);
+                }
+                loadSource(fileId, res, mime, language);
+            }).fail(function() {
+                if (Modernizr.localstorage) {
+                    localStorage.setItem(fileId, "");
+                    loadSource(fileId, "", file.mime, file.language);
+                }
+            });
+        }
     }
     function openFileWithId(fileId) {
         var elementId = fileId + "_anchor";
@@ -303,17 +421,80 @@ var App = function() {
         } else {
             ans = [
                 {'name': '~/', 'type': 'folder', 'id': 'tree-root', 'parent': '#'},
-                {'name': 'sample2.cpp', 'language': 'C++', 'mime': 'text/x-c++src', 'id': 'sample2', 'type': 'file', 'parent': 'tree-root'},
-                {'name': 'sample.cpp', 'language': 'C++', 'mime': 'text/x-c++src', 'id': 'sample', 'type': 'file', 'parent': 'tree-root'},
-                {'name': 'helloworld.cpp', 'language': 'C++', 'mime': 'text/x-c++src', 'id': 'helloworld', 'type': 'file', 'parent': 'tree-root'}
+                {'file': 'sample2.cpp', 'name': 'sample2.cpp', 'language': 'C++', 'mime': 'text/x-c++src', 'id': 'sample2', 'type': 'file', 'parent': 'tree-root'},
+                {'file': 'sample.cpp', 'name': 'sample.cpp', 'language': 'C++', 'mime': 'text/x-c++src', 'id': 'sample', 'type': 'file', 'parent': 'tree-root'},
+                {'file': 'helloworld.cpp', 'name': 'helloworld.cpp', 'language': 'C++', 'mime': 'text/x-c++src', 'id': 'helloworld', 'type': 'file', 'parent': 'tree-root'}
             ];
         }
         makeTree(ans);
     }
     function saveFiles() {
         if (Modernizr.localstorage) {
+            //console.log(files);
             localStorage.setItem('files',JSON.stringify(files));
         }
+    }
+    function saveSource() {
+        if (currentFileId) {
+            if (Modernizr.localstorage) {
+                localStorage.setItem(currentFileId, codeMirror.getValue());
+            }
+        }
+    }
+    function makeNewNodeData(isFolder, parentId) {
+        var newNodeName = isFolder ? "New Folder" : "New File";
+        var alreadyExists = {};
+        files.forEach(function(elem, inex, arr) {
+            if (elem.parent == parentId) {
+                if (elem.name.startsWith(newNodeName)) {
+                    alreadyExists[elem.name] = true;
+                }
+            }
+        });
+        if (alreadyExists[newNodeName]) {
+            for (var i=1;; i++) {
+                var nameCandidate = newNodeName + ' (' + i + ')';
+                if (alreadyExists[nameCandidate]) {
+                } else {
+                    newNodeName = nameCandidate;
+                    break;
+                }
+            }
+        }
+        var temp = {file: newNodeName, name: newNodeName, id: uuid(), type: (isFolder ? 'folder' : 'file'), parent: parentId};
+        if (!isFolder) {
+            temp.language = 'Text';
+            temp.mime = 'text/plain';
+        }
+        files.push(temp);
+        saveFiles();
+        return convertToTreeData(temp);
+    }
+    function createNode(data, isFolder) {
+        var ref = null;
+        if (data) {
+            ref = data.reference;
+        } else {
+            ref = $('#sidebar-tree').jstree(true).get_selected();
+        }
+        var inst = $.jstree.reference(ref),
+            obj = inst.get_node(ref);
+        while(obj.data.type == 'file') {
+            obj = inst.get_node(inst.get_parent(obj));
+        }
+        var nodeData = makeNewNodeData(isFolder, obj.id);
+        inst.create_node(obj, nodeData, "last", function (new_node) {
+            setTimeout(function () { inst.edit(new_node); },0);
+        });
+
+    }
+    function randomString(length, chars) {
+        var result = '';
+        for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+        return result;
+    }
+    function uuid() {
+        return randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
     }
     return {
         init: function() {
--- static/assets/string-utils.js
+++ static/assets/string-utils.js
@@ -3,3 +3,9 @@ if (!String.prototype.includes) {
         return String.prototype.indexOf.apply(this, arguments) !== -1;
     };
 }
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position) {
+        position = position || 0;
+        return this.lastIndexOf(searchString, position) === position;
+    };
+}
