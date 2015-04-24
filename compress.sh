365acbf7a1b48b2b1d230a8546d486d6061a9837
Baekjoon <greenfreakzilla@gmail.com>
2015-04-24 20:12:46 +0900

    add uglifycss

--- compress.sh
+++ compress.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 uglifyjs static/assets/app.js -m -o static/assets/app.min.js
 uglifyjs static/assets/app.js -c -m -o static/assets/app.compress.js
+uglifycss static/assets/style.css > static/assets/style.min.css
